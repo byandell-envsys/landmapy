35509b41ec94552de4fb6ea5e36ef2e0e320d6bd
byandell <byandell@wisc.edu>
2024-11-22 16:32:34 -0600

    add process routines

---
+++ envsys/process_cloud_mask.py
@@ -0,0 +1,56 @@
+import numpy as np # Process bit-wise cloud mask
+
+def process_cloud_mask(cloud_uri, bounds_gdf, bits_to_mask):
+    """
+    Load an 8-bit Fmask file and process to a boolean mask
+
+    Parameters
+    ----------
+    uri: file-like or path-like
+      Fmask file accessor downloaded or obtained from earthaccess
+    bounds_gdf: gpd.GeoDataFrame
+      Area of interest to crop to
+    bits_to_mask: list of int
+      The indices of the bits to mask if set
+
+    Returns
+    -------
+    cloud_mask: np.array
+      Cloud mask
+    """
+    # Open fmask file
+    fmask_da = process_image(cloud_uri, bounds_gdf)
+
+    # Unpack the cloud mask bits
+    cloud_bits = (
+        np.unpackbits(
+            (
+                # Get the cloud mask as an array...
+                fmask_da.values
+                # ... of 8-bit integers
+                .astype('uint8')
+                # With an extra axis to unpack the bits into
+                [:, :, np.newaxis]
+            ), 
+            # List the least significant bit first to match the user guide
+            bitorder='little',
+            # Expand the array in a new dimension
+            axis=-1)
+    )
+
+    cloud_mask = np.sum(
+        # Select bits
+        cloud_bits[:,:,bits_to_mask], 
+        # Sum along the bit axis
+        axis=-1
+    # Check if any of bits are true
+    ) == 0
+    
+    return cloud_mask
+
+# blue_da = process_image(denver_files[1], denver_redlining_gdf)
+# denver_cloud_mask = process_cloud_mask(
+#     denver_files[-1],
+#     denver_redlining_gdf,
+#     [1, 2, 3, 5])
+# blue_da.where(denver_cloud_mask).plot()
