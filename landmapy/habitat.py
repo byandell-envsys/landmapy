231412814406b82a310dc1325b9b782b3a06fd32
byandell <byandell@wisc.edu>
2024-12-12 13:12:10 -0600

    add maca routines to habitat

--- landmapy/habitat.py
+++ landmapy/habitat.py
@@ -1,4 +1,12 @@
 def create_data_dir(new_dir='habitat'):
+    """
+    Create Data Directory if it does not exist.
+
+    Parameters
+    ----------
+    new_dir: character string
+        Name of new directory
+    """
     import os
     import pathlib
 
@@ -20,15 +28,15 @@ def soil_url_dict(place_gdf, soil_var="sand", soil_sum="mean", soil_depth="100_2
     
     Parameters
     ----------
-        place_gdf: GeoDataFrame
-            GeoDataFrame of selected location
-        soil_var, soil_sum, soil_depth: character string
-            Name of soil variable, summary and depth
+    place_gdf: GeoDataFrame
+        GeoDataFrame of selected location
+    soil_var, soil_sum, soil_depth: character string
+        Name of soil variable, summary and depth
 
     Results
     -------
-        soil_url: dict
-            Dictionary of URLs
+    soil_url: dict
+        Dictionary of URLs
     """
     from math import floor, ceil
     
@@ -68,16 +76,16 @@ def merge_soil(place_gdf, soil_var="sand", soil_sum="mean", soil_depth="100_200"
 
     Parameters
     ----------
-        place_gdf: GeoDataFrame
-            GeoDataFrame of selected location
-        soil_var, soil_sum, soil_depth: character string
-            Name of soil variable, summary and depth
-        buffer: number
-            Buffer around bounds of place_gdf
+    place_gdf: GeoDataFrame
+        GeoDataFrame of selected location
+    soil_var, soil_sum, soil_depth: character string
+        Name of soil variable, summary and depth
+    buffer: number
+        Buffer around bounds of place_gdf
     Results
     -------
-        soil_merged_das: DataFrame
-            DataFrame with soil estimates restricted to bounds of place_gdf 
+    soil_merged_das: DataFrame
+        DataFrame with soil estimates clipped to bounds of place_gdf 
     """
     import geopandas as gpd
     import rioxarray as rxr
@@ -101,16 +109,44 @@ def merge_soil(place_gdf, soil_var="sand", soil_sum="mean", soil_depth="100_200"
     soil_merged_das = merge_arrays(soil_das) 
 
     # Use bounds from place_gdf extended by buffer
-    bounds = place_gdf.to_crs(soil_merged_das.rio.crs).total_bounds
-    bounds = bounds + [x * buffer for x in [-1,-1,1,1]] # buffer around place_gdf
-    print(bounds)
-    soil_merged_das = soil_merged_das.rio.clip_box(*bounds)
+    #bounds = place_gdf.to_crs(soil_merged_das.rio.crs).total_bounds
+    #bounds = bounds + [x * buffer for x in [-1,-1,1,1]] # buffer around place_gdf
+    #print(bounds)
+    #soil_merged_das = soil_merged_das.rio.clip_box(*bounds)
+    soil_merged_das = da_bounds(place_gdf, soil_merged_das, buffer)
 
     return soil_merged_das
 
 # soil_merged_das = merge_soil(place_gdf, "sand", "mean", "100_200", 0.1)
 
-def process_maca(sites, scenarios=['pr'], climates=['rcp85', 'rcp45'], years = [2026]):
+def da_bounds(place_gdf, da, buffer = 0.1):
+    """
+    Clip bounds from place_gdf on da extended by buffer.
+
+    Parameters
+    ----------
+    place_gdf: GeoDataFrame
+        GeoDataFrame of selected location
+    da: DataFrame
+        DataFrame from calling routine
+    buffer: number
+        Buffer around bounds of place_gdf
+    Results
+    -------
+    da: DataFrame
+        DataFrame with restricted to bounds of place_gdf 
+    """
+    bounds = place_gdf.to_crs(da.rio.crs).total_bounds
+    bounds = bounds + [x * buffer for x in [-1,-1,1,1]] # buffer around place_gdf
+    da = da.rio.clip_box(*bounds)
+
+    return da
+
+# da_bounds(place_gdf, da, 0.1)
+
+
+def process_maca(sites, scenarios=['pr'], climates=['rcp85', 'rcp45'], years = [2026],
+                 buffer = 0.1):
     """
     Process MACA Monthly Data.
 
@@ -124,6 +160,8 @@ def process_maca(sites, scenarios=['pr'], climates=['rcp85', 'rcp45'], years = [
         'rcp' = relative concentration pathway
     years: numeric
         first year of 5-year period
+    buffer: number
+        Buffer around bounds of place_gdf
 
     Returns
     -------
@@ -160,8 +198,7 @@ def process_maca(sites, scenarios=['pr'], climates=['rcp85', 'rcp45'], years = [
                         lat = ("lat", [convert_lonlat(l) for l in maca_da.lat.values]))
                     maca_da = maca_da.rio.set_spatial_dims(x_dim='lon', y_dim='lat')
                     # Clip bounds.
-                    bounds = site_gdf.to_crs(maca_da.rio.crs).total_bounds
-                    maca_da = maca_da.rio.clip_box(*bounds)
+                    maca_da = da_bounds(site_gdf, maca_da, buffer)
                     maca_da_list.append(dict(
                         site_name = site_name,
                         scenario = scenario,
@@ -173,4 +210,34 @@ def process_maca(sites, scenarios=['pr'], climates=['rcp85', 'rcp45'], years = [
 
     return maca_df
 
-# maca_df = process_maca({'buffalo': buffalo_gdf}, ['pr'], ['rcp85', 'rcp45'], [2026])
+# maca_df = process_maca({'buffalo': buffalo_gdf}, ['pr'], ['rcp85', 'rcp45'], [2026], 0.1)
+
+def maca_year(maca_df, row=0, year=2027):
+    """
+    Extract and print year data
+
+    Parameters
+    ----------
+    maca_df: DataFrame
+        DataFrame with MACA data by row
+
+    Returns
+    -------
+    maca_year: DataArray
+        DataArray for year and row selected.
+    """
+    maca_da = maca_df.loc[row, 'da']
+    # Find the average precipitation for each pixel across all months for each individual year?
+    maca_yearly_da= maca_da.groupby('time.year').mean()
+    
+    # Calculate the total annual precipitation for each year?
+    # maca_annual = maca_yearly_da.groupby('year').sum(["lat", "lon"])
+
+    maca_year = maca_yearly_da['year' == year]
+    maca_year = maca_year.rio.write_crs("EPSG:4326")
+
+    return maca_year
+    
+# maca_2027 = maca_year(maca_df, 0, 2027)
+# from landmapy.index import redline_over_index
+# redline_over_index(buffalo_gdf, maca_2027, edgecolor="white")
